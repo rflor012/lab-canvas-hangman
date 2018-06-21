0b93348aebca7d60d30fb8279785c48ecec6c9fa
Rodrigo Flores <rflor012@gmail.com>
2018-06-21 17:49:57 -0400

    some changes

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,51 @@
+function draw() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+
 }
 
-HangmanCanvas.prototype.createBoard = function () {
 
-};
+// function HangmanCanvas(secretWord) {
+//   var canvas = document.getElementById('hangman');
+//
+//
+// }
+//
+// HangmanCanvas.prototype.createBoard = function () {
 
-HangmanCanvas.prototype.drawLines = function () {
 
-};
 
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+  /* beginPath()    //Creates a new path. Once created, future drawing commands are directed into the path and used to build the path up.
+closePath()    //Closes the path so that future drawing commands are once again directed to the context.
+stroke()      //Draws the shape by stroking its outline.
+fill()        //Draws a solid shape by filling the path's content area.*/
+//
+// };
 
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
+// HangmanCanvas.prototype.drawLines = function () {
+//
+// };
+//
+// HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+//
+// };
+//
+// HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+//
+// };
+//
+// HangmanCanvas.prototype.drawHangman = function (shape) {
+//
+// };
+//
+// HangmanCanvas.prototype.gameOver = function () {
+//
+// };
+//
+// HangmanCanvas.prototype.winner = function () {
+//
+// };
